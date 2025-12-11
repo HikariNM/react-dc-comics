040a228e8e7340fffbe4f43976b0dff9c9934945
HikariNM <nicole.hikari.mascellino@gmail.com>
2025-12-11 21:26:18 +0100

    eliminato fascione blu

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,31 +5,6 @@ function Footer() {
     return (
         <>
             <footer>
-                {/* <div className={style.container}>
-                    <ul className={style.shopLink}>
-                        <li>
-                            <img src="./src/assets/buy-comics-digital-comics.png" alt="" />
-                            <a href="#"> DIGITAL COMICS </a>
-                        </li>
-                        <li>
-                            <img src="./src/assets/buy-comics-merchandise.png" alt="" />
-                            <a href="#"> DC MERCHANDISE </a>
-                        </li>
-                        <li>
-                            <img src="./src/assets/buy-comics-subscriptions.png" alt="" />
-                            <a href="#"> SUBSCIPTION </a>
-                        </li>
-                        <li>
-                            <img src="./src/assets/buy-comics-shop-locator.png" alt="" />
-                            <a href="#"> COMIC SHOP LOCATOR </a>
-                        </li>
-                        <li>
-                            <img src="./src/assets/buy-dc-power-visa.svg" alt="" />
-                            <a href="#"> DC POWER VISA </a>
-                        </li>
-
-                    </ul>
-                </div> */}
                 <div className={style.footerNavLinkContainer}>
                     <div className={style.footerLogo}>
                         <div className={style.leftNavLink}>
