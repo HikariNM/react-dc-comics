61a19a83e5fe52d0cfb468200c408e5d5ef41f67
HikariNM <nicole.hikari.mascellino@gmail.com>
2025-12-14 16:24:14 +0100

    realizzato main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,33 @@
 import style from "./Main.module.css";
 import MiddleSection from './MiddleSection'
+import card from './comics'
 
 function Main() {
 
     return (
         <>
             <main>
-                <h2 className={style.content}>--&gt; Content goes here &lt;-- </h2>
+                {/* <h2 className={style.content}>--&gt; Content goes here &lt;-- </h2> */}
+                <div className={style.jumboContainer}>
+                    <span className={`${style.btn} ${style.pageTitle}`}>CURRENT SERIES</span>
+                </div>
+
+                <div className={style.container}>
+                    <div className={style.cardsContainer}>
+                        {
+                            card.map(card => {
+                                return <div className={style.card}>
+                                    <img src={card.thumb} alt={card.title} />
+                                    <p>{card.series.toUpperCase()}</p>
+                                </div>
+                            })
+                        }
+                        <button className={`${style.loadBtn} ${style.btn}`}>LOAD MORE</button>
+                    </div>
+                </div>
+
+
+
                 <MiddleSection />
             </main>
         </>
